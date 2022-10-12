66d03b3c3e2b2ccc8ee44b7ddd876a69b92cea41
Denis Kuniss <kuniss@grammarcraft.de>
2022-10-12 22:23:58 +0200

    Adjusted tests for issue #1 testing.

--- test/issues/issue-gh-1.d
+++ test/issues/issue-gh-1.d
@@ -2,12 +2,23 @@ module test.issues.issue_gh_1;
 
 import test.helper;
 
-@("test for GH issue #1")
+@("test GH issue #1 for unproductive start symbol")
 unittest
 {
     with (sandbox)
     {
         run!"cat test/issues/issue-gh-1.eag | ./gamma --output-directory %s"(directory)
-            .shouldFailNotMatching("Error: undefined identifier P0");
+            .shouldFailNotMatching("Error: undefined identifier P0")
+            .shouldFail("error: start symbol Expr is unproductive");
+    }
+}
+
+@("test GH issue #1 for unproductive non-start symbol")
+unittest
+{
+    with (sandbox)
+    {
+        run!"cat test/issues/issue-gh-1-warn-only.eag | ./gamma --output-directory %s"(directory)
+            .shouldMatch("warn: ExprTail unproductive");
     }
 }
